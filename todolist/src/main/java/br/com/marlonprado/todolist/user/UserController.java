833961cacc917386eeb035b834553398352d0679
Marlon Prado <marlonprado04@gmail.com>
2023-10-10 15:41:48 -0300

    feat: add ResponseEntity for bad and sucess request

--- todolist/src/main/java/br/com/marlonprado/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/marlonprado/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.marlonprado.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +17,27 @@ public class UserController {
 
     // Método para criar usuário através do corpo da requisição
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
 
         // Buscando usuário no banco de dados pelo username
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         // Verifica se busca retornou algum usuário
         if(user != null){
+            // Mensagem de erro no console se usuário existir
             System.out.println("Usuário já existe");
-            return null;
+
+            // Retorno de bad_request para a requisição
+            // E mensagem de erro no body da requisição
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         // Salva usuário no banco de dados
         var userCreated = this.userRepository.save(userModel);
 
-        // Retornando usuário após salvar ele no banco de dados
-        return userCreated;
+        // Retornando status de criado para a equisição
+        // E retornando informações do usuário no body da requisição
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
 
